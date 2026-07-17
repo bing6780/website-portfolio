e43f3c497d103def1954aa96db808677fa89be36
bing6780 <hoainamyahooaz@gmail.com>
2026-07-17 07:53:29 +0700

    Add script.js for portfolio loading message

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    console.log("Portfolio của Trần Lê Hoài Nam đã load xong!");
+
+    alert("Chào mừng bạn đến với Portfolio của mình!");
+
+});
